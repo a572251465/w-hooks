0b6f5a88b816a76c3685de36345e3ea7db3b930f
lihaohao <2495041749@qq.com>
2022-07-31 19:11:06 +0800

    feat: requestIdleCallback 实现

---
+++ packages/src/utils/requestIdleCallback.ts
@@ -0,0 +1,58 @@
+import { INormalFn } from '../types'
+import { isFunction } from './isFunction'
+import { isNumber } from './isNumber'
+
+const messageChannel = new MessageChannel()
+const activeFrameTime = 1000 / 60
+const getTime = () => performance.now()
+let frameDeadline = 0
+let pendingCallback: INormalFn | null = null
+const timeRemaining = () => frameDeadline - getTime()
+// 表示是否执行任务
+let idleTimeout: null | NodeJS.Timeout = null
+
+messageChannel.port2.onmessage = () => {
+  const currentTime = getTime()
+  const didTimeout = frameDeadline <= currentTime
+  if (didTimeout || timeRemaining() > 1) {
+    if (idleTimeout) {
+      clearTimeout(idleTimeout)
+      idleTimeout = null
+    }
+    if (pendingCallback && isFunction(pendingCallback))
+      pendingCallback({
+        didTimeout: frameDeadline <= currentTime,
+        timeRemaining
+      })
+  }
+}
+
+const requestIdleCallback = (
+  callback: INormalFn,
+  options: { timeout?: number } = {}
+) => {
+  if (!isFunction(callback)) {
+    console.error(`params callback must be a function`)
+  }
+
+  const { timeout } = options
+  if (isNumber(timeout) && (timeout as number) > 0) {
+    idleTimeout = setTimeout(() => {
+      callback()
+    }, timeout)
+  }
+
+  // rafTime 距离绘制的时间
+  requestAnimationFrame((rafTime) => {
+    pendingCallback = callback
+
+    // 一般情况下一帧是60hz
+    // rafTime 表示到绘制之前的时间
+    // activeFrameTime 表示一帧使用的时间
+    // rafTime + activeFrameTime 就是从触发requestAnimationFrame 到下次绘制之前总共时间
+    frameDeadline = rafTime + activeFrameTime
+    messageChannel.port1.postMessage(null)
+  })
+}
+
+export { requestIdleCallback }
